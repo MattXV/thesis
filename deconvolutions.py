b11bb6f8b2781645b4c97b59c9137baab24da7b3
__matt <mattia.clmb@gmail.com>
2023-09-04 23:17:51 +0100

    real/synth comparisons.

--- deconvolutions.py
+++ deconvolutions.py
@@ -47,30 +47,50 @@ for sr_pair in mono_recordings:
 
 
 # %% RIR metrics
+# %% 45dB SNR
 
-fig, (ax, bx) = plt.subplots(1, 2,  tight_layout=True, dpi=300, figsize=(6.4, 2.2))
+real_rirs = list(Path(r'D:\gdrive\BCU\Research\AcousticRenderingEvaluation\deconvolutions').glob('*.wav'))
+synthetic_rirs = list(Path(r'C:\Users\matt\git\geometrical-acoustics\GeometricalAcoustics\Assets\Irs\acoustic-rendering-eval').glob('*.wav'))
+pairs = list()
 
-real_rirs = list(Path(r'G:\My Drive\BCU\Research\AcousticRenderingEvaluation\deconvolutions').glob('*.wav'))
+pairs = [(i, next(filter(lambda x: x.stem in i.stem, real_rirs))) for i in real_rirs]
 
+#%%
+
+fig, axes = plt.subplots(2, 2,  tight_layout=True, dpi=300, figsize=(6.4, 2.2))
+ax, bx = axes[0, :]
+cx, dx = axes[1, :]
 test, fs = st.sf.read(real_rirs[0])
+test_s, test_s_fs = st.sf.read(synthetic_rirs[0])
+test_s = st.trim_from_to(test_s, 0, 0.5)
 
 t = np.linspace(0, test.shape[0] / fs, test.shape[0])
 # ax.plot(t, test)
 a = np.abs(hilbert(test))
+print(a.shape)
 a = np.convolve(a, np.ones(5001)/5001, mode='valid')
-e = st.mag_to_db(a)
+print(a.shape)
+e = 20 * np.log10(a / np.max(a))
 t = np.linspace(0, e.shape[0] / fs, e.shape[0])
-
+ax.plot(np.linspace(0, test.shape[0] / fs, test.shape[0]), test)
 bx.plot(t, e)
-bx.plot([0, t[-1]], [-30, -30])
+bx.plot([np.argmax(e < -30) / FS], [-30], marker="*", markersize=13)
+print()
+
+a = np.abs(hilbert(test_s))
+a = np.convolve(a, np.ones(5001)/5001, mode='valid')
+e = 20 * np.log10(a / np.max(a))
+
+cx.plot(np.linspace(0, test_s.shape[0] / fs, test_s.shape[0]), test_s)
+dx.plot(np.linspace(0, e.shape[0] / fs, e.shape[0]), e)
+dx.plot([np.argmax(e < -30) / FS], [-30], marker="*", markersize=13)
 # st.plot_spectrogram(test, fs, bx, scale=10)
 
 # rt_60 = st.get_rt60(test, fs)
 # d_50 = st.get_d50(test, fs)
 # c_50 = st.get_c50(test, fs)
 
-lt = 10* np.log10(np.cumsum(a[:np.argmax(a)][::-1]**2) / np.sum(a[:np.argmax(a)] **2) )[::-1]
-ax.plot(lt)
+ax.plot()
 plt.show()
 
 
