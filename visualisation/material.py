6bb78fd299192790ace145aa6562607e9cb4793b
__matt <mattia.clmb@gmail.com>
2023-12-04 13:30:09 +0000

    Setting up local build.

---
+++ visualisation/material.py
@@ -0,0 +1,12 @@
+# %%
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnnotationBbox, OffsetImage
+import scienceplots
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams["text.usetex"] = True
+plt.style.use('science')
+
+
